4fa5e2c739ac6de9fbfc7857e4bc9802f02c0df8
Jeevan0818 <156219266+Jeevan0818@users.noreply.github.com>
2025-07-07 15:02:24 +0530

    Create database.py

---
+++ database.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+# Initialize database
+def init_db(app):
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
